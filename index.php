3be4065d1efa7aeb6677ec1c98b85eef04b6d608
hanh <dovanhanh12b@gmail.com>
2019-08-05 16:43:01 +0700

    create 05.08.19

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'LinkList.php';
+
+$linkList = new LinkList();
+$linkList->insertFirst(11);
+$linkList->insertFirst(22);
+$linkList->insertLast(33);
+$linkList->insertLast(44);
+$linkData = $linkList->readList();
+
+echo 'Total Links: ' . $linkList->totalNode() . '<br>';
+echo implode('-',$linkData);
